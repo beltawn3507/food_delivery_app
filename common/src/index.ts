af84fb27c8e9c3d22dfcaf3f8ce5d6bc3a36dc91
beltawn3507 <152001888+beltawn3507@users.noreply.github.com>
2026-06-21 19:42:23 +0530

    Update

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,16 +1,20 @@
-import cloudinary  from 'cloudinary';
-
 export * from './middleware/cloudinary'
 export * from './middleware/isAuth'
 
-const { CLOUD_NAME, CLOUD_API_KEY, CLOUD_SECRET_KEY } = process.env;
+// middleware/cloudinary.ts
 
-if (!CLOUD_NAME || !CLOUD_API_KEY || !CLOUD_SECRET_KEY) {
-  throw new Error("Missing Cloudinary environment variables");
-}
+import cloudinary from "cloudinary";
 
-cloudinary.v2.config({
-  cloud_name: CLOUD_NAME,
-  api_key: CLOUD_API_KEY,
-  api_secret: CLOUD_SECRET_KEY,
-});
+export const configureCloudinary = () => {
+  const { CLOUD_NAME, CLOUD_API_KEY, CLOUD_SECRET_KEY } = process.env;
+
+  if (!CLOUD_NAME || !CLOUD_API_KEY || !CLOUD_SECRET_KEY) {
+    throw new Error("Missing Cloudinary environment variables");
+  }
+
+  cloudinary.v2.config({
+    cloud_name: CLOUD_NAME,
+    api_key: CLOUD_API_KEY,
+    api_secret: CLOUD_SECRET_KEY,
+  });
+};
